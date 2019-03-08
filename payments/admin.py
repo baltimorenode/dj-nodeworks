0b96df319a31d108ca97f485d03052dbbb77d978
John N. Milner <john@tmoj.net>
2019-03-07 23:00:49 -0500

    Register models with the admin interface

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Donation, Subscription
+
 # Register your models here.
+
+admin.site.register(Donation, admin.ModelAdmin)
+admin.site.register(Subscription, admin.ModelAdmin)
